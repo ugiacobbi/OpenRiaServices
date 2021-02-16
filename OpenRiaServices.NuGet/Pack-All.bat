fa2dd795071ee5f6a8345652575f550bbb444ddb
Pierre Barjon <barjon.pierre@gmail.com>
2021-02-16 13:36:05 +0100

    Add .nuspec for OpenSilver's NuGet package.

---
+++ OpenRiaServices.NuGet/Pack-All.bat
@@ -0,0 +1,2 @@
+..\..\nuget.exe pack "OpenSilver/OpenRiaServices.OpenSilver.Client.Core.nuspec" -OutputDirectory "bin/opensilver/"
+pause
---
+++ OpenRiaServices.NuGet/Pack-Client-OS.ps1
@@ -0,0 +1,31 @@
+param(
+    [string]$Path = ".",
+	[string[]]$Include =  @("OpenRiaServices.OpenSilver.Client.Core.nuspec", "OpenRiaServices.OpenSilver.Client.nuspec"),
+	[string]$Version = $null,
+	[string]$NuGetPath
+)
+
+$scriptPath = (Split-Path -Parent $PSCommandPath)
+Push-Location $scriptPath
+
+$outputDir = "bin/opensilver"
+if (-not (Test-Path $outputDir)) {
+    mkdir $outputDir
+}
+
+# If NuGet path is not specified then chekc one folder above git repo, or hope for it to be in the path
+if ([string]::IsNullOrEmpty($NuGetPath))
+{
+	if (Test-Path ..\..\NuGet.exe) { $NuGetPath = "..\..\NuGet.exe"}
+	else { $NuGetPath = "nuget.exe"}
+}
+[string[]]$NuGetParameters = @("-OutputDirectory", "$outputDir")
+if (-not [string]::IsNullOrEmpty($Version)) {$NuGetParameters = $NuGetParameters + @("-Version", $Version)}
+
+foreach($nuspec in (dir $Path -Recurse -Include $Include))
+{
+        echo "Building $nuspec"
+        & $NuGetPath pack ($nuspec) $NuGetParameters
+}
+
+Pop-Location
