7a8b2c82b18759c63a4aece4133472b61054d55f
Pierre Barjon <barjon.pierre@gmail.com>
2021-09-27 12:26:47 +0200

    Port DomainDataSource control to OpenSilver

--- NuGet/Pack-Client-OpenSilver.ps1
+++ NuGet/Pack-Client-OpenSilver.ps1
@@ -1,6 +1,6 @@
 param(
     [string]$Path = ".",
-	[string[]]$Include =  @("OpenSilver.OpenRiaServices.Client.Core.nuspec", "OpenSilver.OpenRiaServices.Client.nuspec"),
+	[string[]]$Include =  @("OpenSilver.OpenRiaServices.Client.Core.nuspec", "OpenSilver.OpenRiaServices.Client.nuspec", "OpenSilver.OpenRiaServices.DomainDataSource.nuspec"),
 	[string]$Version = $null,
 	[string]$NuGetPath
 )
